3f0ce6c2f92079ce2c0c5b0fbb92e44b6f726ea8
admin <1107504318@qq.com>
2018-04-16 11:10:26 +0800

    输出文件的路径与源文件路径不一致时手动替换路径

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-#npm i gulp-md5-plus gulp jsonfile del gulp-uglify gulp-cssmin gulp-htmlmin --save-dev
-
-## gulpfile.js
-var gulp = require("gulp");
-var md5 = require("gulp-md5-plus");//添加MD5
-var del = require("del");//清除旧版本文件
-var uglify = require("gulp-uglify");//压缩js
-var cssmin = require('gulp-cssmin');
-var htmlmin = require('gulp-htmlmin');//压缩html
+#npm i gulp-md5-plus gulp jsonfile del gulp-uglify gulp-cssmin gulp-htmlmin run-sequence --save-dev
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ var del = require("del");//清除旧版本文件
 var uglify = require("gulp-uglify");//压缩js
 var cssmin = require('gulp-cssmin');
 var htmlmin = require('gulp-htmlmin');//压缩html
+var replace = require('gulp-replace-pro');//替换
+var through = require('through2');
+var runSequence = require('run-sequence');//顺序执行
 // var rename = require("gulp-rename");//文件重命名
+var curr = '';//替换后的路径
 
 gulp.task('clean', function(){
     return del(['./output','./manifest.json'])
@@ -42,33 +46,66 @@ gulp.task('html2',function(){
         .pipe(gulp.dest('./output/'))
 });
 
-gulp.task('css',['html2'],function(){
+gulp.task('css',function(){
     return gulp.src("./style.css")
-        // .pipe(cssmin())
-        .pipe(md5(10,['./output/index.html','./output/home.html'],{
+        .pipe(cssmin())
+        .pipe(md5(10,'',{//由于css输出目录结构改变，所以等下自己替换路径
             mappingFile: 'manifest.json'
         }))
-        .pipe(gulp.dest("./output/"));
-});
-
-gulp.task('js',['css'],function(){
-    return gulp.src("./demo1.js")
-        .pipe(uglify())
-        .pipe(md5(10,'./output/*.html',{
-            mappingFile: 'manifest.json'
+        .pipe(gulp.dest("./output/css"))
+         .pipe(through.obj(function(file,enc,cb){
+            curr = file.relative;//保存输出的文件名，替换路径时使用
+            this.push(file);
+            cb();
         }))
-        .pipe(gulp.dest("./output/"));
 });
 
-gulp.task('img' , ['js'],function() {
-    return gulp.src(['./img.png'])
-        .pipe(md5(10 ,'./output/*.css',{
-            dirLevel:null,
-            mappingFile: 'manifest.json'
-        }))
-        .pipe(gulp.dest('./output/'));
-});
-
-gulp.task('default',['clean'],function(){
-    gulp.start('html','html2','css','js','img');
+gulp.task('cssRe',function(){//替换index.html里css的引入路径
+    return gulp.src('./output/index.html')
+    .pipe(replace({
+        './style.css': './css/'+curr,
+    }))
+    .pipe(gulp.dest('./output/'));
 })
+
+gulp.task('js',function(){
+    return gulp.src("./demo.js")
+        // .pipe(uglify())
+        .pipe(md5(10,'./output/index.html',{
+            mappingFile: 'manifest.json'
+        }))
+        .pipe(gulp.dest("./output/"));
+});
+
+gulp.task('img',function() {
+    return gulp.src(['./img.png'])
+        .pipe(md5(10 ,'',{
+            mappingFile: 'manifest.json'
+        }))
+        .pipe(gulp.dest('./output/'))
+        .pipe(through.obj(function(file,enc,cb){
+            curr = file.relative;
+            this.push(file);
+            cb();
+        }))
+});
+
+gulp.task('imgRe',function(){
+    return gulp.src(['./output/css/*.css'])
+    .pipe(replace({
+        './img.png': '../'+curr,
+    }))
+    .pipe(gulp.dest('./output/css'));
+})
+
+gulp.task('default',function(done){
+    runSequence(
+        ['html'],
+        ['html2'],
+        ['css'],
+        ['cssRe'],
+        ['js'],
+        ['img'],
+        ['imgRe'],
+    done,);
+});
